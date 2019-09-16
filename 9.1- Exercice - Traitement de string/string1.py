70a846d3736611da282a458eb91a728c669007f6
Stefane Fivaz <s.a.fivaz@gmail.com>
2019-09-16 09:37:43 +0200

    exercice 9.1

--- 9.1- Exercice - Traitement de string/string1.py
+++ 9.1- Exercice - Traitement de string/string1.py
@@ -1,2 +1,13 @@
 # Consigne : Récupérer le mot "chaine" du string s et l'afficher
 s = 'un exemple de chaine'
+
+# Pourquoi l'IDE reclame de scope Monsieur?
+def getSubString(string, start, end):
+    substring = ""
+    for letter in range(start, end):
+        substring = substring + string[letter]
+    return substring
+
+
+substring = getSubString(s, 14, 20)
+print(substring)
