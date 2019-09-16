83c0a6fa288c25791433be582eb0a6cfc2f8a673
Stefane Fivaz <s.a.fivaz@gmail.com>
2019-09-16 09:56:25 +0200

    exercice 11.1

--- 11.1- Exercice - Boucle/boucle1.py
+++ 11.1- Exercice - Boucle/boucle1.py
@@ -2,3 +2,14 @@
 # Données : un tableau A de n nombre réels
 # Résultat attendu : Moyenne des nombres réels du tableau A
 A = [1, 5, 15, 25, 10, 55, 50, 35]
+
+
+def get_average(number_array):
+    sum = 0
+    for number in number_array:
+        sum += number
+
+    return sum / len(number_array)
+
+
+print("Moyenne des nombres réels du tableau A", get_average(A), sep=" : ")
