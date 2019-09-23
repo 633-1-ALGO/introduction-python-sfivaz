81fc10dadb0eb36504742522b8cb7f30ab1eee96
Fivaz Stefane (HES) <stefane.fivaz@hes.adhes.hesge.ch>
2019-09-23 09:52:59 +0200

    13.1

--- 13.1- Exercice - Fonctions/fonction.py
+++ 13.1- Exercice - Fonctions/fonction.py
@@ -4,3 +4,14 @@ texte = "ceci est un texte que vous pouvez modifier mais gare aux caracteres spe
 tab_lettres = [
     ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
      'x', 'y', 'z', ' '], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+def frequence_lettres(texte, tab_lettres):
+    for lettre in texte:
+        index = 0
+        while index < len(tab_lettres[0]):
+            if lettre == tab_lettres[0][index]:
+                tab_lettres[1][index] += 1
+            index += 1
+    print(tab_lettres[1])
+
+frequence_lettres(texte, tab_lettres)
