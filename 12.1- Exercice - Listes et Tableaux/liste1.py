e7fec9eb0b380f64c199e20e4bc7440927d4f136
Fivaz Stefane (HES) <stefane.fivaz@hes.adhes.hesge.ch>
2019-09-23 09:11:28 +0200

    12.1

--- 12.1- Exercice - Listes et Tableaux/liste1.py
+++ 12.1- Exercice - Listes et Tableaux/liste1.py
@@ -6,3 +6,8 @@
 # Indication :   L'alignement rectiligne n'est pas une contrainte, tant que la table est visible ligne par ligne c'est ok.
 #               Si vous êtes perfectionnistes faites vous plaisir.
 liste = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for number in liste:
+    for number2 in liste:
+        print(number * number2, end=" ")
+    print("")
