9979bedd5cbbdf1dea349c90219285a89edf9ca1
Fivaz Stefane (HES) <stefane.fivaz@hes.adhes.hesge.ch>
2019-09-23 09:20:10 +0200

    12.2

--- 12.2- Exercice - Listes et Tableaux/liste2.py
+++ 12.2- Exercice - Listes et Tableaux/liste2.py
@@ -2,3 +2,16 @@
 # Résultat attendu : Un affichage comme ceci : "La valeur maximum est :  x  et elle se trouve à l'indice [ n ][ m ]
 
 tab = [[4, 7, 3, 20, 42], [2, 4, 5, 7, 2], [23, 24, 15, 75, 23]]
+
+plus_grand = 0
+i = 0
+j = 0
+while i < len(tab):
+    while j < len(tab[i]):
+        if tab[i][j] > plus_grand:
+            plus_grand = tab[i][j]
+        j = j + 1
+    i = i + 1
+    j = 0
+
+print(plus_grand)
